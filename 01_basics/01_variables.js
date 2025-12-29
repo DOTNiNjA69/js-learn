cc4049ae99b6107024f105700068a0d7dbe399a5
Sanjay_Thomas_569 <sanjaythomas7100@gmail.com>
2025-12-29 17:40:12 +0000

    container code config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="sanjaythomas7100gmail.com"
+var accountPass="12345"
+accountCity="Kochi"
+let accountState;
+
+// accountId=28930
+
+accountEmail="anaghamone@gmail.com"
+accountPass="9872"
+accountCity="Bengaluru"
+console.log(accountState);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; //    would be treated as new standard version of js
+//alert(3+3) cant be used in node.js. it works in browser
+//code readability should be high
+//numer ->.  2^(53)
+//bigInt
+//string "    "
+//boolean true or false 
+//null => standalone value
+//undefined  =>.   value not assiged yet
+//symbol  => uniqueness
+
+// object 
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
