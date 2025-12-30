d77e3d040d86ab9b8bff85ad6f1ea7e0a76aff9c
Sanjay_Thomas_569 <sanjaythomas7100@gmail.com>
2025-12-30 05:07:04 +0000

    comparison code

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,38 @@
+// let score ="33"
+// //type can also be determined by console.log((typeof score))
+// //score was given in string but now it gets converted
+// let ValueInNumber= Number (score)
+// console.log(typeof ValueInNumber)
+// let kundi="33 abc"
+// let kundiInNumber= Number (kundi)
+// console.log( kundiInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1.    false =>0 
+
+
+let scur= 0
+let scurInBool= Boolean (scur)
+// console.log(scurInBool)
+
+// 1 -> true
+//0-> false
+//"sanjay -> true"
+
+
+//if a number is converted nto stringf format then itt gets converted into string format
+//strings get added
+let str1 = "sanjay"
+let str2 = " says hello"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("2"+"3");
+// console.log("2"+3+3);
+// console.log(3+2+"4")
+// console.log(+true);
+let gameCounter=100
+console.log(++gameCounter);
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+//null comparisons doesnt give expected result
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+//equality check works in other way than comaprison
+// strict check => checks value as well as datatype
+
+//array
+const heros  = ["shaktiman","superman","batman"]
+//objects are written under curly braces {
+//}
+
+let myObj = {
+    name : "sanjay",
+    age : 21 ,
+}
+
+//function => is treated like a variable in js 
+
+const myFunction = function()
+{
+    console.log("hello world");
+    
+}
