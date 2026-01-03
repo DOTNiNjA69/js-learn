aa49cacbf120711aceca04ad2db39190e7c74097
Sanjay_Thomas_569 <sanjaythomas7100@gmail.com>
2026-01-03 03:28:31 +0000

    lec 12

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name="sanjay"
+const repoCount=50
+console.log(`my name is ${name} and my repocount is ${repoCount}`);
+const gameName =new String('sanjaythomas')
+console.log(gameName.charAt(6));
+console.log(gameName.indexOf('t'));
+
